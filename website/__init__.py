b0062bb3d7d61f527e0a23546fb97add1fbfcfed
Sachin Pokharel <sachinwork33@gmail.com>
2023-01-18 09:44:19 +0530

    Flask simple project

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+# Setup our Flask Application
+from flask import Flask
+from .views import views
+from .auth import auth
+
+
+def create_app():
+    app = Flask(__name__)
+    # Config variable encrypt cookies or sessions for the website
+    app.config['SECRET_KEY'] = 'flaskApp'
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
